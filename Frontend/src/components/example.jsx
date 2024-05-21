5df32d5ae5f4403dd36ffc8c45d39c49a87fb1ef
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-05-20 22:43:28 -0400

    :construction: Creating boilerplate

---
+++ Frontend/src/components/example.jsx
@@ -0,0 +1,9 @@
+// Pueden eliminar el archivo si es necesario
+// O utilizarlo cambiando el nombre
+// Tanto del archivo como del componente 😉
+
+const Example = () => {
+	return <h1 className="text-3xl font-bold">Example</h1>
+}
+
+export default Example
---
+++ Frontend/src/views/App.jsx
@@ -0,0 +1,7 @@
+import Example from '../components/example'
+
+function App() {
+	return <Example />
+}
+
+export default App
