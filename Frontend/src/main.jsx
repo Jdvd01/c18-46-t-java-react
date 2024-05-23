af6d4852ea2f1512141ad0b4eeec861a0b05d3a6
quserforgitp <adealumnonegligas@gmail.com>
2024-05-23 14:26:35 -0600

    feat(app): :heavy_plus_sign: agregado react router
    se agrego la dependencia y se comentaron los archivos afectados con instrucciones de como se usa

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './views/App.jsx'
-import './styles/index.css'
-import { store } from './redux/store.js'
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./views/App.jsx";
+import "./styles/index.css";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 // Este archivo deberia ser solo de configuracion
 // Routers, providers y demas
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<Provider store={store}>
-		<App />
-	</Provider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>{/* Las rutas estan definidas en App.jsx */}
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
--- Frontend/src/views/App.jsx
+++ Frontend/src/views/App.jsx
@@ -1,7 +1,18 @@
 import Example from '../components/example'
+import { Routes, Route } from 'react-router-dom';
+
 
 function App() {
-	return <Example />
+	return (
+		<Routes>
+			<Route path="/" element={<Example />} />
+			{/* 
+			Agregar mas rutas aqui...
+				Ejemplo
+				<Route path="/registro" element={<Registro />} />
+			 */}
+		</Routes>
+	);
 }
 
 export default App
