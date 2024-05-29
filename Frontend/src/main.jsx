ca86769212d40d2893a3599c34fc1e030311fe92
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-05-29 18:56:46 -0400

    Navbar created

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,17 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./views/App.jsx";
-import "./styles/index.css";
-import { store } from "./redux/store.js";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './views/App.jsx'
+import './styles/index.css'
+import { store } from './redux/store.js'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import { Navbar } from './components/Navbar.jsx'
 
 // Este archivo deberia ser solo de configuracion
 // Routers, providers y demas
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>{/* Las rutas estan definidas en App.jsx */}
-      <App />
-    </BrowserRouter>
-  </Provider>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<Provider store={store}>
+		<BrowserRouter>
+			<Navbar />
+
+			{/* Las rutas estan definidas en App.jsx */}
+			<App />
+		</BrowserRouter>
+	</Provider>
+)
