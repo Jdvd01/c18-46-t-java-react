34c9a7a12bfc052a931b2a98c394c98e78491b10
AlfredDev <alfredgarcia14200@gmail.com>
2024-05-29 16:11:58 -0600

    Navegacion por catalogo y busqueda de libros por concidencias hecho, tarejeta: US03

--- Backend/src/main/java/com/booklyn/Backend/Services/BookService.java
+++ Backend/src/main/java/com/booklyn/Backend/Services/BookService.java
@@ -19,7 +19,7 @@ public interface BookService {
     BookDTO getBookById(Long id);
 
     void deleteBookById(Long id);
-    //buesqueda por conditions
+    //buesqueda por conditions de libros
     Page<BookDTO> findBooksByCriteria(String title, String author, String genre, Pageable pageable);
 
 }
