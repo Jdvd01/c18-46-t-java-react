9a3b28b38a131ad4b03b26cb53b1e0751a9fb580
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-05-29 18:52:52 -0400

    Error page created

--- Frontend/src/views/App.jsx
+++ Frontend/src/views/App.jsx
@@ -1,6 +1,6 @@
 import Example from '../components/example'
-import { Routes, Route } from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom'
+import { ErrorPage } from './404'
 
 function App() {
 	return (
@@ -11,8 +11,9 @@ function App() {
 				Ejemplo
 				<Route path="/registro" element={<Registro />} />
 			 */}
+			<Route path="*" element={<ErrorPage />} />
 		</Routes>
-	);
+	)
 }
 
 export default App
