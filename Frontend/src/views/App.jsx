7237dd124e85760ef01515ae430b11fc59271c11
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-05-29 19:15:35 -0400

    Delimiting the grid

--- Frontend/src/views/App.jsx
+++ Frontend/src/views/App.jsx
@@ -4,15 +4,18 @@ import { ErrorPage } from './404'
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Example />} />
-			{/* 
-			Agregar mas rutas aqui...
-				Ejemplo
-				<Route path="/registro" element={<Registro />} />
-			 */}
-			<Route path="*" element={<ErrorPage />} />
-		</Routes>
+		// Delimitaciones del grid 👇🏽
+		<div className="mx-8 desktop:mx-40">
+			<Routes>
+				<Route path="/" element={<Example />} />
+				{/* 
+				Agregar mas rutas aqui...
+					Ejemplo
+					<Route path="/registro" element={<Registro />} />
+				*/}
+				<Route path="*" element={<ErrorPage />} />
+			</Routes>
+		</div>
 	)
 }
 
