40f05ce895e23f2927988419622b70d31dafb570
quserforgitp <adealumnonegligas@gmail.com>
2024-06-06 13:55:54 -0600

    feat(auth): :sparkles: add logout functionality navbar button
    cuando hay una de sesion usuario activa, al ser clickeado el boton de login del navbar la finaliza

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,67 +1,69 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 // Assets
-import { LoginSVG } from '../assets/svg/LoginSVG.jsx'
-import { MenuSVG } from '../assets/svg/MenuSVG.jsx'
-import { CartSVG } from '../assets/svg/CartSVG.jsx'
-import { HomeSVG } from '../assets/svg/HomeSVG.jsx'
+import { LoginSVG } from "../assets/svg/LoginSVG.jsx";
+import { MenuSVG } from "../assets/svg/MenuSVG.jsx";
+import { CartSVG } from "../assets/svg/CartSVG.jsx";
+import { HomeSVG } from "../assets/svg/HomeSVG.jsx";
+
+import useAuth from "../hooks/useAuth.js";
 
 export const Navbar = () => {
-	const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
 
-	const token = false
+  const { token, dispatchLogout } = useAuth();
 
-	return (
-		<div className="bg-primary-500 text-white flex justify-between items-center flex-wrap tablet:flex-nowrap p-4 tablet:py-5 tablet:px-8 fixed top-0 min-w-full z-50">
-			<Link to={'/'}>
-				<img src="" alt="Logo" />
-			</Link>
-			<button className="flex tablet:hidden" onClick={() => setShow(!show)}>
-				<MenuSVG height={'24'} width={'24'} color={'#FFFFFF'} />
-			</button>
-			<div
-				className={`${
-					show ? 'flex' : 'hidden'
-				} tablet:flex w-full justify-start tablet:justify-end gap-2 pt-2`}
-			>
-				<ul className={`${show ? 'block' : 'hidden'} tablet:flex gap-8`}>
-					<li>
-						<Link to={'/'} className="flex gap-2 items-center">
-							<HomeSVG height={'24'} width={'24'} color={'#FFFFFF'} />
-							<span className="font-inter text-body-1 tablet:hidden desktop:block">
-								Home
-							</span>
-						</Link>
-					</li>
-					<li>
-						<Link to={'/placeholder'} className="flex gap-2 items-center">
-							<CartSVG height={'24'} width={'24'} color={'#FFFFFF'} />
-							<span className="font-inter text-body-1 tablet:hidden desktop:block">
-								Cart
-							</span>
-						</Link>
-					</li>
-					<li>
-						{token ? (
-							<Link to={'/'} className="flex gap-2 items-center">
-								<LoginSVG height={'24'} width={'24'} color={'#FFFFFF'} />
-								<span className="font-inter text-body-1 tablet:hidden desktop:block">
-									Log out
-								</span>
-							</Link>
-						) : (
-							<Link to={'/auth'} className="flex gap-2 items-center">
-								<LoginSVG height={'24'} width={'24'} color={'#FFFFFF'} />
-								<span className="font-inter text-body-1 tablet:hidden desktop:block">
-									Log in
-								</span>
-							</Link>
-						)}
-					</li>
-				</ul>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="bg-primary-500 text-white flex justify-between items-center flex-wrap tablet:flex-nowrap p-4 tablet:py-5 tablet:px-8 fixed top-0 min-w-full z-50">
+      <Link to={"/"}>
+        <img src="" alt="Logo" />
+      </Link>
+      <button className="flex tablet:hidden" onClick={() => setShow(!show)}>
+        <MenuSVG height={"24"} width={"24"} color={"#FFFFFF"} />
+      </button>
+      <div
+        className={`${
+          show ? "flex" : "hidden"
+        } tablet:flex w-full justify-start tablet:justify-end gap-2 pt-2`}
+      >
+        <ul className={`${show ? "block" : "hidden"} tablet:flex gap-8`}>
+          <li>
+            <Link to={"/"} className="flex gap-2 items-center">
+              <HomeSVG height={"24"} width={"24"} color={"#FFFFFF"} />
+              <span className="font-inter text-body-1 tablet:hidden desktop:block">
+                Home
+              </span>
+            </Link>
+          </li>
+          <li>
+            <Link to={"/placeholder"} className="flex gap-2 items-center">
+              <CartSVG height={"24"} width={"24"} color={"#FFFFFF"} />
+              <span className="font-inter text-body-1 tablet:hidden desktop:block">
+                Cart
+              </span>
+            </Link>
+          </li>
+          <li>
+            {token ? (
+              <Link onClick={() => dispatchLogout()} to={"/"} className="flex gap-2 items-center">
+                <LoginSVG height={"24"} width={"24"} color={"#FFFFFF"} />
+                <span className="font-inter text-body-1 tablet:hidden desktop:block">
+                  Log out
+                </span>
+              </Link>
+            ) : (
+              <Link to={"/auth"} className="flex gap-2 items-center">
+                <LoginSVG height={"24"} width={"24"} color={"#FFFFFF"} />
+                <span className="font-inter text-body-1 tablet:hidden desktop:block">
+                  Log in
+                </span>
+              </Link>
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
