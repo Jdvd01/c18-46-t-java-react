29400f01722756266419ffd751549d1f9b9f2120
Rafael Toresani <rtoresani@gmail.com>
2024-06-10 21:02:01 -0300

    cambios paginacion review - save review - findbook

--- Backend/src/main/java/com/booklyn/Backend/Services/ReviewService.java
+++ Backend/src/main/java/com/booklyn/Backend/Services/ReviewService.java
@@ -3,6 +3,7 @@ package com.booklyn.Backend.Services;
 import com.booklyn.Backend.DTO.Requests.ReviewRequest;
 import com.booklyn.Backend.Models.Reviews.Review;
 import org.apache.coyote.BadRequestException;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -10,9 +11,8 @@ public interface ReviewService {
 
     public Review cretateReview(Long bookId, Long userId, ReviewRequest reviewRequest);
 
-    public List<Review> findReviewsByBookId(Long bookId);
-
-    public List<Review> findReviewsByUser(Long userId);
+    Page<Review> findReviewsByBookId(Long bookId, int page, int size);
+    Page<Review> findReviewsByUser(Long userId, int page, int size);
 
 
     public Review removeReviewById(Long reviewId);
