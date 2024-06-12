628282cf62db2a3eaf8be5f189ae4c1700917fb4
Jose Velasquez <velasquezdenorajose@gmail.com>
2024-06-12 16:17:45 -0400

    Add review redux

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -3,6 +3,7 @@ import counterReducer from './reducers/example_counter/counterSlice'
 import exampleReducer from './reducers/example/exampleSlice'
 import authReducer from './reducers/auth/authSlice'
 import bookReducer from './reducers/book/bookSlice'
+import reviewReducer from './reducers/review/reviewSlice'
 
 export const store = configureStore({
 	reducer: {
@@ -10,5 +11,6 @@ export const store = configureStore({
 		example: exampleReducer,
 		auth: authReducer,
 		book: bookReducer,
+		review: reviewReducer,
 	},
 })
